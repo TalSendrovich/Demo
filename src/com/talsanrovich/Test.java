93120e097364f1ba44a38cfc122c180da3f2a728
Tal Sandrovich <talsandrovich@gmail.com>
2020-09-25 14:31:06 +0300

    Added test class

---
+++ src/com/talsanrovich/Test.java
@@ -0,0 +1,11 @@
+package com.talsanrovich;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class Test {
+    private int grade = 100;
+
+    public void printme() {
+        System.out.println(grade);
+    }
+}
